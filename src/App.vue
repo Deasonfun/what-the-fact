75a23a3410bf68adaa8766cb5041324f901aa88c
Deasonfun <dftothemax@gmail.com>
2023-05-21 11:42:22 -0400

    Fact now displays on main page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <TestComp />
+  <div class="card">
+    <div class="card-header">
+      Fact:
+    </div>
+    <div class="card-body">
+      <p class="card-text">{{ this.fact }}</p>
+      <a href="#" class="btn btn-success">Next</a>
+    </div>
+  </div>
 </template>
 
 <script>
-import TestComp from './components/TestComp.vue'
-import axios from 'axios'
+  import axios from 'axios';
 
-export default {
-  name: 'what-the-fact',
-  components: {
-    TestComp
-  },
-  async mounted() {
-    axios.get('http://localhost:8080/test')
-      .then(res => console.log(res))
+  export default {
+    name: 'what-the-fact',
+    components: {
+    },
+    data() {
+      return {
+        fact: ''
+      }
+    },
+    async mounted() {
+      axios.get('http://localhost:8080/test')
+        .then(res => this.fact = res.data.fact)
+    }
+   
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
