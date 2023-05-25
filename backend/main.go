417049d4af2e8fc381bdb1237a53f8b15621398f
Deasonfun <dftothemax@gmail.com>
2023-05-25 16:26:08 -0400

    Backend now queries SQL database

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	err := godotenv.Load()
+	CheckError(err)
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+
+	connectString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	BuildDB(connectString)
+
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
@@ -27,23 +46,57 @@ func main() {
 	client := &http.Client{}
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"fact": GetFact(*client),
+			"fact": GetFact(*client, connectString),
 		})
 	})
 
-	r.Run()
+	r.Run(":8080")
 }
 
-func GetFact(client http.Client) (fact string) {
-	req, _ := http.NewRequest("GET", "https://api.api-ninjas.com/v1/facts?limit=1", nil)
-	req.Header.Set("X-Api-Key", "6Ioq8wPk4a8CSiaBhn7lUw==W3UHGZPwOGyCT5z2")
-	res, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(res.Body)
-	reqData := string(body)
-	data := strings.Split(reqData, ":")
-	factData := strings.Split(data[1], ":")
-	factString := strings.Split(factData[0], "}")
-	fmt.Println(strings.TrimLeft(factString[0], "\""))
-	fact = strings.TrimLeft(factString[0], "\"")
-	return
+func BuildDB(connectString string) {
+
+	db, err := sql.Open("postgres", connectString)
+	CheckError(err)
+
+	defer db.Close()
+	err = db.Ping()
+	CheckError(err)
+	fmt.Println("Connected to database...")
+
+	factJSON, err := ioutil.ReadFile("./facts.json")
+	CheckError(err)
+
+	var payload map[string]interface{}
+	err = json.Unmarshal([]byte(factJSON), &payload)
+	CheckError(err)
+
+	fmt.Println(payload["fact"])
+	fmt.Println("")
+
+	_, err = db.Exec(fmt.Sprintf("INSERT INTO facts (fact) VALUES('%s')", payload["fact"]))
+	CheckError(err)
+}
+
+func GetFact(client http.Client, connectString string) (fact string) {
+	var (
+		queryFact string
+	)
+	db, err := sql.Open("postgres", connectString)
+	CheckError(err)
+	rows, err := db.Query("select fact from facts where id=1")
+	CheckError(err)
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&queryFact)
+		CheckError(err)
+	}
+	err = rows.Err()
+	CheckError(err)
+	return queryFact
+}
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
