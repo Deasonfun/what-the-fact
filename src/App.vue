71d4241acf56c72963aab6e36dfe077df9fbcb92
Deasonfun <dftothemax@gmail.com>
2024-02-14 08:44:56 -0500

    Fixed fact card sizing

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <div class="card">
-    <div class="card-header">
-      Fact:
-    </div>
-    <div class="card-body">
-      <p class="card-text">{{ this.fact }}</p>
-      <button @click="NextFact" class="btn btn-success">Next</button>
+  <div class="fact-card-container">
+    <div class="card">
+      <div class="card-header">
+        Fact:
+      </div>
+      <div class="card-body">
+        <p class="card-text">{{ this.fact }}</p>
+        <button @click="NextFact" class="btn btn-success">Next</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
-
 
   export default {
     name: 'what-the-fact',
@@ -46,5 +47,16 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+  .fact-card-container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 40rem;
+  }
+  .card {
+    width: 20rem;
+    height: 100%;
   }
 </style>
